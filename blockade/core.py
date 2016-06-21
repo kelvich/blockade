697c0652a015ee45c031563cfad997ddf6c4b5b5
Stas Kelvich <stas.kelvich@gmail.com>
2016-06-21 15:05:00 +0300

    set ulimit -c

--- blockade/core.py
+++ blockade/core.py
@@ -131,6 +131,7 @@ class Blockade(object):
             dns=container.dns,
             port_bindings=port_bindings,
             network_mode=network_mode,
+            ulimit='core=1000000',
             links=links)
 
         def create_container():
